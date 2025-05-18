0100148d239cbe258ec691d167f18d73c5f34807
Zac <zac.serocki@gmail.com>
2025-05-17 23:13:32 -0600

    version V1.1

--- initialize.py
+++ initialize.py
@@ -39,8 +39,7 @@ def get_personal_path():
 def write_jsons():
     # Make .vscode folder
     vscode_path = ".vscode"
-    if os.path.isdir(vscode_path) : shutil.rmtree(vscode_path)
-    os.mkdir(vscode_path)
+    if not os.path.isdir(vscode_path) : os.mkdir(vscode_path)
 
     # Make settings.json
     settings_json_path = vscode_path + "/settings.json"
@@ -51,7 +50,7 @@ def write_jsons():
     # Make vex_project_settings.json
     vex_project_settings_path = vscode_path + "/vex_project_settings.json"
     vps = open(vex_project_settings_path, "w")
-    vex_project_settings_json = '{\n\t"extension": {\n\t\t"version": "0.6.0",\n\t\t"json": 2\n\t},\n\t"project": {\n\t\t"name": "' + '",\n\t\t"description": "",\n\t\t"creationDate": "",\n\t\t"platform": "V5",\n\t\t"language": "python",\n\t\t"slot": 1,\n\t\t"sdkVersion": "V5_1_0_1_19",\n\t\t"python": {\n\t\t\t"main": "compiled/main.py"\n\t\t}\n\t}\n}'
+    vex_project_settings_json = '{\n\t"extension": {\n\t\t"version": "0.6.0",\n\t\t"json": 2\n\t},\n\t"project": {\n\t\t"name": "' + '",\n\t\t"description": "",\n\t\t"creationDate": "",\n\t\t"platform": "V5",\n\t\t"language": "python",\n\t\t"slot": 1,\n\t\t"sdkVersion": "V5_1_0_1_25",\n\t\t"python": {\n\t\t\t"main": "compiled/main.py"\n\t\t}\n\t}\n}'
     vps.write(vex_project_settings_json)
 
 write_jsons()
